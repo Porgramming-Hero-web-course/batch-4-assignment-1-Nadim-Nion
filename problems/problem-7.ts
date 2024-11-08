2e0d07592e9dbe16f3ef08ed8ffcca89143a8525
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-11-08 23:04:26 +0600

    problem 7 solved

--- problems/problem-7.ts
+++ problems/problem-7.ts
@@ -2,6 +2,24 @@
     class Car {
         make: string;
         model: string;
-        year: number;
+        year: number
+
+        constructor(make: string, model: string, year: number) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        getCarAge() {
+            if (this.year < 2024) {
+                return 2024 - this.year;
+            }
+            else {
+                return 2024;
+            }
+        }
     }
+
+    const car = new Car("Honda", "Civic", 2018);
+    console.log(car.getCarAge());
 }
