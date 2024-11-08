4da56b34610887baedcd3c18fc5db70f85fc2e03
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-11-08 21:06:18 +0600

    problem 3 solved

---
+++ problems/problem-3.ts
@@ -0,0 +1,14 @@
+{
+    const countWordOccurrences = (sentence: string, word: string) => {
+        const smallLetterSentence = sentence.toLowerCase();
+        const smallLetterWord = word.toLowerCase();
+
+        const wordsArray = smallLetterSentence.split(' ');
+        const matchedCount = wordsArray.filter(item => item === smallLetterWord).length;
+
+        return matchedCount;
+    }
+
+    const result = countWordOccurrences('I love typescript', 'typescript');
+    console.log(result);
+}
